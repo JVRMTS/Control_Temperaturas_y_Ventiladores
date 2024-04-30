6b8229677e42ec85b6c93c24072a63a98679769d
JMTS <74095359+JVRMTS@users.noreply.github.com>
2024-04-30 08:09:45 +0200

    Ajustes de retroiluminación

--- Calendario_Temperatura_Humedad_LCD/retroiluminacion.h
+++ Calendario_Temperatura_Humedad_LCD/retroiluminacion.h
@@ -1,27 +1,30 @@
 /* 
  * Control de la retroiluminación de la pantalla, mediante la lectura de una LDR
- * Escribimos en PWM con la libreria Esp32 AnalogWrite que hay que modificar
+ * Escribimos en PWM con la libreria PWMOutESP32
  * @JMTS - www.jmts.es
- * Creado el 26/12/2023
+ * Creado el 28/04/2024
  */
 
 // Asignamos los pines de la iluminación del LCD y del LDR y declaramos sus variables
-const int pinLCD = 18;
-const int pinLDR = 32;
+#define PINLCD 18
+#define PINLDR 32
+// Definimos el minimo de retroiluminación
+#define MINIMO 8
 unsigned int valorLDR;
 unsigned int valorLCD;
 
+
 #include <PWMOutESP32.h>
-PWMOutESP32 pwm;
+PWMOutESP32 pwm(10,5000); // Ajustamos la resolución de 12 bits y una frecuencia de 5000Hz
 
 void iluminacion(){
 // Comprobamos la intensidad de la luz
-    valorLDR = analogRead(pinLDR); // Leemos la variable del LDR
-    unsigned int x = valorLDR/2;
-    if(x < 6){
-      valorLCD = 5; // esto es para que nunca se apaque del todo el LCD
-    }else{
-      valorLCD = x; // Asignamos el valor de iluminación del LCD en base al valor del LDR
-    }
-    pwm.analogWrite(pinLCD, valorLCD); // Escribimos en el pinLCD el valor de la retroiluminación
+  valorLDR = analogRead(PINLDR)/4; // Leemos la variable del LDR
+// Comprobamos una intenisad lumínica mínima para que no se apague nunca el LCD
+  if(valorLDR <= MINIMO){
+    valorLCD = MINIMO;        // Ajustamos un mínimo de iluminación
+  }else{
+    valorLCD = valorLDR;  // Asignamos el valor de iluminación del LCD en base al valor del LDR
+  }
+  pwm.analogWrite(PINLCD, valorLCD); // Escribimos en el pinLCD el valor de la retroiluminación
 }
